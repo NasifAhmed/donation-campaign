0220b740b78c978d582c6f6408ae2958d42284da
Nasif Ahmed <nasif2ahmed@gmail.com>
2023-09-27 21:57:55 +0600

    Add navbar

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,25 @@
-import React from "react";
+import React, { Children } from "react";
 import ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import App from "./App.jsx";
+import Donation from "./components/Donation.jsx";
+import Statistics from "./components/Statistics.jsx";
 import "./index.css";
 
 const router = createBrowserRouter([
     {
         path: "/",
         element: <App></App>,
+        children: [
+            {
+                path: "/donation",
+                element: <Donation></Donation>,
+            },
+            {
+                path: "/statistics",
+                element: <Statistics></Statistics>,
+            },
+        ],
     },
 ]);
 
