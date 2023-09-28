1f9264a52dd8c503efb3a25a1c49f8f7703376ce
Nasif Ahmed <nasif2ahmed@gmail.com>
2023-09-28 22:23:45 +0600

    Add statistics page

--- src/components/Statistics.jsx
+++ src/components/Statistics.jsx
@@ -1,9 +1,72 @@
-import React from "react";
+import React, { useState } from "react";
+import {
+    PieChart,
+    Pie,
+    Sector,
+    Cell,
+    ResponsiveContainer,
+    Legend,
+} from "recharts";
+import { getDonation } from "../utility/localStorage";
+
+const COLORS = ["#FF444A", "#00C49F"];
+
+const RADIAN = Math.PI / 180;
+const renderCustomizedLabel = ({
+    cx,
+    cy,
+    midAngle,
+    innerRadius,
+    outerRadius,
+    percent,
+    index,
+}) => {
+    const radius = innerRadius + (outerRadius - innerRadius) * 0.5;
+    const x = cx + radius * Math.cos(-midAngle * RADIAN);
+    const y = cy + radius * Math.sin(-midAngle * RADIAN);
+
+    return (
+        <text
+            x={x}
+            y={y}
+            fill="white"
+            textAnchor={x > cx ? "start" : "end"}
+            dominantBaseline="central"
+            className="text-4xl font-bold"
+        >
+            {`${(percent * 100).toFixed(0)}%`}
+        </text>
+    );
+};
 
 const Statistics = () => {
+    const [donationCount, setDonationCount] = useState(getDonation().length);
+
+    const data = [
+        { name: "Total Donation", value: 12 },
+        { name: "Your Donation", value: donationCount },
+    ];
+
     return (
         <div>
-            <h1>Statistics</h1>
+            <ResponsiveContainer width="100%" height="100%" aspect={3}>
+                <PieChart width={400} height={400}>
+                    <Pie
+                        data={data}
+                        dataKey="value"
+                        cx="50%"
+                        cy="50%"
+                        fill="#8884d8"
+                        labelLine={false}
+                        label={renderCustomizedLabel}
+                    >
+                        {data.map((entry, index) => (
+                            <Cell fill={COLORS[index % COLORS.length]} />
+                        ))}
+                    </Pie>
+                    <Legend verticalAlign="bottom" height={36} />
+                </PieChart>
+            </ResponsiveContainer>
         </div>
     );
 };
